4b517d873e4a951db0bcee8869373d32c0c34f07
Jack <103317213+J-S-Harris@users.noreply.github.com>
2022-05-04 22:05:01 +0100

    V 0.3
    Rave mode! Can parse up to 10 colours from 1 tweet.
    Yeelight API rate limit gets hit though, among other bugs/tweaks to make. Includes lots of extra things to add in the future!

--- Twitter Lamp Control.py
+++ Twitter Lamp Control.py
@@ -1,4 +1,4 @@
-    # TO IMPLEMENT?:
+    # TO IMPLEMENT AND MENTION ON WEBSITE:
 # 1) Tweet out the sender's twitter handle, and action taken, when a command tweet is received.
 
 # 2) Add time+date AND colour of each tweet to a dictionary
@@ -6,14 +6,18 @@
 
 # 3) Post everywhere! Molten twitter, UK Melee discord, etc
 
-# 4) allow it to check for multiple colours, (incl. rgb), and cycle through all of them!
-    # total ~16 seconds, i.e: 16/len(rgb+if_r+if_g+if_b)
+# 4) Allow multiple colours per tweet
+    #(DONE! 10 per tweet; disco mode)
 
 # 5) Make it parse RGB with regex!
 
-# 6) Update website with a few words per capability
+# 6) Update website with a few words per 
 
-# 7) Why rate limit????
+# 7) Optimise the final wait for as little down time as poss
+
+# 8) Currently skips tweets??
+
+# 9) Why does the Bulb API max out after only a few colours? How does it work/what is the rate limit????
 
 
 # TWEEPY METHODS:
@@ -28,11 +32,10 @@ import yeelight
 import time
 import re
 
-all_mentions =[]
-
-bulb = yeelight.Bulb(configfile.bulb_IP, effect='smooth',duration=3000)
+bulb = yeelight.Bulb(configfile.bulb_IP, effect='smooth',duration=1000)
 bulb.turn_on()
 bulb.set_brightness(30)
+bulb.set_rgb(0,100,100)
 
 my_bearer_token = configfile.my_bearer_token
 consumer_key=configfile.API_key
@@ -40,96 +43,103 @@ consumer_secret=configfile.API_secret_key
 access_token=configfile.accesstoken
 access_token_secret=configfile.accesstokensecret
 
+all_senders = []
+all_dates = []
+all_mentions =[]
+colour_duration=''
+colours_list=''
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 # 'parser=' optional, but makes it return JSON rather than gibberish
 
-def spam():
-    x=0
-    while True:
-        time.sleep(3)
-        x+=1
-        api.update_status(x)
-#spam()
 
-def find_text():
-    x=api.user_timeline()
-    y=x[0]['text']
-    print(y)
-    if 'Test 1' in y:
-        print('Found')
-        bulb.set_rgb(255,0,0)
-        time.sleep(1)
-        bulb.set_rgb(0,255,0)
-#find_text()
+# api.update_status(x)
+    # ^ Tweets 'x'
 
-
-def check_own_new_tweet():  # Activates every time Lampy tweets
-    x=api.user_timeline() # pulls data re: OWN tweets
-    most_recent_text=x[0]['text']  # pulls account's most recent tweet's text
-    most_recent_date=x[0]['created_at'] # pulls most recent tweet's date
-    print(most_recent_date, most_recent_text)
-
-    time.sleep(3)
-    x=api.user_timeline() # Waits 3s then checks own tweets again...
-    if most_recent_date != x[0]['created_at'] and most_recent_text != x[0]['text']:
-        bulb.set_rgb(0,0,255) # if new tweet found, turns blue...
-        time.sleep(3)
-        bulb.set_rgb(255,0,0) # ...then red 3s later
-    else: bulb.set_rgb(255,0,0) # Red is the default colour
-
-#while True: 
-#    check_own_new_tweet()
-
-
+# Main function:
 def check_mentions():
-    x=api.mentions_timeline()
-        # Make it so that a list of mentions is collected,
-        # so no tweet gets missed.
-        # Tweet out a thank you, pulling handle from JSON, and explaining what was done.
-    most_recent_mention=x[0]['text']
-    most_recent_mention_date=x[0]['created_at']
-    time.sleep(15)
+
+    global all_senders
+    global all_dates
+    global all_mentions
+    
+    x=api.mentions_timeline() # establishes initial list of mentions
+
+    for i in x:
+        all_senders.append(i['user']['screen_name'])
+        all_dates.append(i['created_at'])
+        all_mentions.append(i['text'])
+
     while True:
-        print(most_recent_mention, most_recent_mention_date)
-        time.sleep(15)
-        x=api.mentions_timeline(count=1)
-        global all_mentions
-        if x[0]['text'] not in all_mentions:
-            all_mentions.append(x[0]['text'])
-        bulb.turn_on()
-        if most_recent_mention != x[0]['text'] and most_recent_mention_date != x[0]['created_at']:
-            bulb.set_brightness(80)
-            colours_list=[]
-            if 'rgb' in x[0]['text'].lower():
-                pass
-                #use a regex and read that from the tweet!!
-            if 'red' in x[0]['text'].lower():
-                bulb.set_rgb(255,0,0)   # Change each of these lines to add respective colour to colours_list...
-            if 'blue' in x[0]['text'].lower():
-                bulb.set_rgb(0,0,255)
-            if 'green' in x[0]['text'].lower():
-                bulb.set_rgb(0,255,0)
-            # ... then strobe them all for 16/colours seconds.
-            time.sleep(12)  # Get rid of this?
-            bulb.set_brightness(30)
-            bulb.set_rgb(255,255,255)
-            # Make it check here if there are any tweets in 'all_mentions' that haven't been flashed,
-                # ^ and ONLY THEN get more (reduce API calls)
-        x=api.mentions_timeline(count=1)
-        most_recent_mention=x[0]['text']
-        most_recent_mention_date=x[0]['created_at']
         
+        global colour_duration
+        global colours_list
+        
+        x=api.mentions_timeline()    # refreshes list of most recent mentions
+        for i in x:
+            if i['created_at'] not in all_dates:
 
-check_mentions()
+                all_senders.append(i['user']['screen_name'])
+                all_dates.append(i['created_at'])
+                all_mentions.append(i['text'])            
 
-def post_to_self():    
-    client = tweepy.Client(consumer_key=API_key,
-        consumer_secret=API_secret_key,
-        access_token=accesstoken,
-        access_token_secret=accesstokensecret)
-    client.create_tweet(text='Hi!')  # Tweets to authorised account:
+                bulb.turn_on()
+                bulb.set_brightness(80) # Flash brightness
+
+                global colours_list
+                colours_list=[]
+
+                most_recent_mention = i['text']
+                most_recent_mention = most_recent_mention.split()
+                for z in most_recent_mention:
+                    print('z:',z)
+                    if z == 'red':
+                        colours_list.append('red')
+                    elif z == 'blue':
+                        colours_list.append('blue')
+                    elif z == 'green':
+                        colours_list.append('green')
+                    #elif 'rgb' in z:
+                        # regex to find colours directly between 'rgb(' and ')'
+                print(colours_list)
+                if len(colours_list) == 0:
+                    colours_list.append('white')
+                global colour_duration
+                colour_duration = 16/int(len(colours_list))
+                print('colour duration assigned:',colour_duration)
+
+                x=0
+                for z in colours_list:
+                    if x<10:
+                        x+=1
+                        print(x)
+                        if z == 'red':
+                            bulb.set_rgb(255,0,0)   # WHY DOES API MAX OUT? Increase the colour_duration integer??? [what is the limit? not 60a minute???
+                            time.sleep(colour_duration)
+                        elif z == 'blue':
+                            bulb.set_rgb(0,0,255)
+                            time.sleep(colour_duration)
+                        elif z == 'green':
+                            bulb.set_rgb(0,255,0)
+                            time.sleep(colour_duration)
+                        elif z == 'white':
+                            bulb.set_rgb(200,200,200)
+                            time.sleep(colour_duration)
+                        #elif ',' in z:
+                            # regex to see if it is (num,num,num)                            
+                            #bulb.set_rgb(z)
+                            #time.sleep(colour_duration)
+                    print('end of colours flashed?')
+        
+            bulb.set_brightness(30) # Resting brightness
+            bulb.set_rgb(0,100,100)
+
+        print('\nT E S T: end of function\n\n',all_senders,'\n\n',all_dates,'\n\n',all_mentions,'\n\n') # TEST SCRIPT
+        
+        
+        time.sleep(20-(2*len(colours_list))) # Should always lead to 26s cycles (16 lit up)
 
 if __name__ == '__main__':
-    pass
+    check_mentions()
